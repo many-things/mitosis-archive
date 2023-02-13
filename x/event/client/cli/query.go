b6ab4bded016e79cdb084052f4bd4878dd80348e
byeongsu-hong <hong@byeongsu.dev>
2023-02-13 18:39:23 +0000

    add event queries

--- x/event/client/cli/query.go
+++ x/event/client/cli/query.go
@@ -25,6 +25,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand(CmdVoteStatus())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
