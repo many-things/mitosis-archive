1234d88c5c209e82cb45f510cb777edb88cd6f0c
hashableric <hashableric@gmail.com>
2023-02-22 14:16:09 +0900

    feat: gen types from pb

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -21,9 +21,9 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./mitosis -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdgepth 1 -name '*.proto'); do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
       buf generate $file
     fi
