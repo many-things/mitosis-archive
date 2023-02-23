f64bd70946f5f01c348aa470b827bc2dcba4f878
hashableric <hashableric@gmail.com>
2023-02-23 15:34:54 +0900

    feat: multisig manager

---
+++ sidecar/run.go
@@ -0,0 +1,36 @@
+package sidecar
+
+import (
+	"context"
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/client"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/many-things/mitosis/sidecar/config"
+	"github.com/many-things/mitosis/sidecar/tofnd"
+	"github.com/many-things/mitosis/sidecar/types"
+
+	"github.com/tendermint/tendermint/libs/log"
+	"google.golang.org/grpc"
+	"time"
+)
+
+func connectGrpc(host string, port string, timeout time.Duration, logger log.Logger) (*grpc.ClientConn, error) {
+	serverAddr := fmt.Sprintf("#{host}:#{port}")
+	logger.Info(fmt.Sprintf("initial connection to tofnd server: #{serverAddr}"))
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	return grpc.DialContext(ctx, serverAddr, grpc.WithInsecure(), grpc.WithBlock())
+}
+
+func createTofNManager(cliCtx client.Context, config config.SidecarConfig, logger log.Logger, valAddr sdk.ValAddress) *tofnd.Manager {
+	conn, err := connectGrpc(config.TofNConfig.Host, config.TofNConfig.Port, config.TofNConfig.DialTimeout, logger)
+	if err != nil {
+		panic(sdkerrors.Wrapf(err, "failed to create multisig manager"))
+	}
+	logger.Debug("successful connection to tofnd gRPC server")
+
+	return tofnd.NewManager(types.NewMultisigClient(conn), cliCtx, valAddr, logger, config.TofNConfig.DialTimeout)
+}
