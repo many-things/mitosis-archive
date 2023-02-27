5721c848100194dd9f7c0864051ae1ec035c64ac
byeongsu-hong <hong@byeongsu.dev>
2023-02-27 08:35:05 +0000

    unify query

--- x/event/keeper/query.go
+++ x/event/keeper/query.go
@@ -1,7 +1,33 @@
 package keeper
 
 import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/many-things/mitosis/x/event/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var _ types.QueryServer = Keeper{}
+
+func (k Keeper) Params(goCtx context.Context, req *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	return &types.QueryParamsResponse{Params: k.GetParams(ctx)}, nil
+}
+
+func (k Keeper) VoteStatus(goCtx context.Context, req *types.QueryVoteStatusRequest) (*types.QueryVoteStatusResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Process the query
+	_ = ctx
+
+	return &types.QueryVoteStatusResponse{}, nil
+}
