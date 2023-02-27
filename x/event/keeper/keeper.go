407459e3832b061a9a635c812fd849d1bc846ebe
byeongsu-hong <hong@byeongsu.dev>
2023-02-27 10:00:51 +0000

    store events

--- x/event/keeper/keeper.go
+++ x/event/keeper/keeper.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"bytes"
+	"encoding/hex"
 	"fmt"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 
@@ -47,12 +48,71 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
 }
 
-func (k Keeper) VoteEvent(ctx sdk.Context, msg *types.MsgVoteEvent) error {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte("vote"))
-	height := ctx.BlockHeight()
+const (
+	PrefixVoteIncomingEvent = "vie"
+	PrefixVoteOutgoingEvent = "vio"
+)
 
-	key := bytes.Join([][]byte{sdk.Uint64ToBigEndian(uint64(height))}, []byte(":"))
-	store.Set(key, value)
+func must[expected any](e expected, err error) expected {
+	if err != nil {
+		panic(err.Error())
+	}
+	return e
+}
+
+func unwrap[expected any](f func() (expected, error)) expected {
+	return must(f())
+}
+func unwrap1[expected any, arg1 any](f func(arg1) (expected, error), a1 arg1) expected {
+	return must(f(a1))
+}
+func unwrap2[expected any, arg1 any, arg2 any](f func(arg1, arg2) (expected, error), a1 arg1, a2 arg2) expected {
+	return must(f(a1, a2))
+}
+
+func (k Keeper) handleIncomingEvent(ctx sdk.Context, evts []*types.IncomingEvent) error {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(PrefixVoteIncomingEvent))
+	height := uint64(ctx.BlockHeight())
+
+	for _, evt := range evts {
+		// ensure txHash already validated before execute this logic
+		key := bytes.Join([][]byte{
+			sdk.Uint64ToBigEndian(height),
+			unwrap1(hex.DecodeString, evt.GetTxHash()),
+			sdk.Uint64ToBigEndian(evt.EventIndex),
+		}, []byte(":"))
+
+		store.Set(key, unwrap(evt.Marshal))
+	}
+
+	return nil
+}
+
+func (k Keeper) handleOutgoingEvent(ctx sdk.Context, evts []*types.OutgoingEvent) error {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(PrefixVoteOutgoingEvent))
+	height := uint64(ctx.BlockHeight())
+
+	for _, evt := range evts {
+		// ensure txHash already validated before execute this logic
+		key := bytes.Join([][]byte{
+			sdk.Uint64ToBigEndian(height),
+			unwrap1(hex.DecodeString, evt.GetTxHash()),
+		}, []byte(":"))
+
+		store.Set(key, unwrap(evt.Marshal))
+	}
+
+	return nil
+}
+
+func (k Keeper) VoteEvent(ctx sdk.Context, msg *types.MsgVoteEvent) error {
+	if err := k.handleIncomingEvent(ctx, msg.GetIncoming()); err != nil {
+		return err
+	}
+
+	if err := k.handleOutgoingEvent(ctx, msg.GetOutgoing()); err != nil {
+		return err
+	}
 
 	return nil
 }
