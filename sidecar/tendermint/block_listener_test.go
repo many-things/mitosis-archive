232b30b25429e44f4f34385b809576a518ecbf1c
hashableric <hashableric@gmail.com>
2023-03-01 21:10:16 +0900

    fix: mutex

--- sidecar/tendermint/block_listener_test.go
+++ sidecar/tendermint/block_listener_test.go
@@ -4,19 +4,31 @@ import (
 	"context"
 	coretypes "github.com/tendermint/tendermint/rpc/core/types"
 	"gotest.tools/assert"
+	"sync"
 	"testing"
 	"time"
 )
 
 type mockHTTP struct {
 	Height int64
+	lock   sync.Mutex
 }
 
-func (m mockHTTP) BlockchainInfo(ctx context.Context, minHeight int64, maxHeight int64) (*coretypes.ResultBlockchainInfo, error) {
+func (m *mockHTTP) BlockchainInfo(ctx context.Context, minHeight int64, maxHeight int64) (*coretypes.ResultBlockchainInfo, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
 	return &coretypes.ResultBlockchainInfo{
 		LastHeight: m.Height,
 		BlockMetas: nil,
 	}, nil
+}
+
+func (m *mockHTTP) ChangeHeight(newHeight int64) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.Height = newHeight
 }
 
 func Test_GetLatestBlockHeight(t *testing.T) {
@@ -34,12 +46,12 @@ func Test_GetBlockHeight(t *testing.T) {
 	mHttp := mockHTTP{
 		Height: 10,
 	}
-	blockListener := NewBlockListener(context.Background(), &mHttp, time.Millisecond*500)
+	blockListener := NewBlockListener(context.Background(), &mHttp, time.Second)
 
 	heightChan, _ := blockListener.GetBlockHeight()
 	var i int64
 	for i = 10; i < 13; i++ {
-		mHttp.Height = i
+		mHttp.ChangeHeight(i)
 		select {
 		case elem := <-heightChan:
 			assert.Equal(t, i, elem)
@@ -56,7 +68,7 @@ func Test_GetNewBlock(t *testing.T) {
 
 	newBlockChan, _ := blockListener.NewBlockWatcher()
 	time.Sleep(time.Second)
-	mHttp.Height = 20
+	mHttp.ChangeHeight(20)
 
 	var i int64
 	for i = 11; i <= 20; i++ {
