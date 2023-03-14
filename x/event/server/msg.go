7cc5f1ac982c8024513b08d54a73b53689674a44
byeongsu-hong <hong@byeongsu.dev>
2023-03-14 09:38:33 +0000

    event: rename msg server

--- x/event/server/msg.go
+++ x/event/server/msg.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"github.com/many-things/mitosis/x/event/keeper"
 )
 
@@ -10,4 +11,19 @@ type msgServer struct {
 
 func NewMsgServer(keeper keeper.Keeper) MsgServer {
 	return msgServer{keeper}
+}
+
+func (m msgServer) Submit(ctx context.Context, submit *MsgSubmit) (*MsgSubmitResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m msgServer) Vote(ctx context.Context, vote *MsgVote) (*MsgVoteResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m msgServer) RegisterProxy(ctx context.Context, proxy *MsgRegisterProxy) (*MsgRegisterProxyResponse, error) {
+	//TODO implement me
+	panic("implement me")
 }
