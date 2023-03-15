9386bab7b39230347d9f106171fea5587212d528
byeongsu-hong <hong@byeongsu.dev>
2023-03-15 05:02:18 +0000

    chore: cleanup

--- x/event/types/keys.go
+++ x/event/types/keys.go
@@ -1,9 +1,5 @@
 package types
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
 const (
 	// ModuleName defines the module name
 	ModuleName = "event"
@@ -17,29 +13,3 @@ const (
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey = "mem_event"
 )
-
-// PREFIX
-var (
-	PrefixVoteIncomingEvent = []byte{0x00}
-	PrefixVoteOutgoingEvent = []byte{0x01}
-
-	PrefixValidatorProxy = []byte{0x10}
-)
-
-func GetIncomingEventPrefix(height uint64) []byte {
-	return append(
-		PrefixVoteIncomingEvent,
-		sdk.Uint64ToBigEndian(height)...,
-	)
-}
-
-func GetOutgoingEventPrefix(height uint64) []byte {
-	return append(
-		PrefixVoteOutgoingEvent,
-		sdk.Uint64ToBigEndian(height)...,
-	)
-}
-
-func GetValidatorProxyPrefix() []byte {
-	return PrefixValidatorProxy
-}
