c1136ec8d59b33ed46beab2927f0fc88d2842518
ByeongSu Hong <hong@byeongsu.dev>
2023-03-21 12:43:28 +0900

    feat(x/event): implement submit vote (#81)
    * merge: feat/event -> base
    
    * pkg: functional
    
    * fix(pkg/types)/ map logic and use it in mapkv
    
    * fix(proto): multi-poll submit
    
    * config: change coverage output type
    
    * event: implement submit / vote event

--- x/event/keeper/poll.go
+++ x/event/keeper/poll.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	mitotypes "github.com/many-things/mitosis/pkg/types"
 	"github.com/many-things/mitosis/x/event/keeper/state"
@@ -9,11 +10,14 @@ import (
 )
 
 type PollKeeper interface {
-	// SubmitPoll handles [server.MsgSubmit]
-	SubmitPoll(ctx sdk.Context, val sdk.ValAddress, chain string, events []*types.Event) (uint64, error)
+	// FilterNewPolls specifies fresh poll and returns new poll and existing polls' index
+	FilterNewPolls(ctx sdk.Context, chain string, polls []*types.Poll) ([]*types.Poll, []mitotypes.KV[uint64, []byte], error)
 
-	// VotePoll handles [server.MsgVote]
-	VotePoll(ctx sdk.Context, val sdk.ValAddress, chain string, ids []uint64) error
+	// SubmitPolls handles [server.MsgSubmit] - returns key value set of poll_id and event_hash
+	SubmitPolls(ctx sdk.Context, chain string, polls []*types.Poll, totalPower, valPower sdk.Int) ([]mitotypes.KV[uint64, []byte], error)
+
+	// VotePolls handles [server.MsgVote]
+	VotePolls(ctx sdk.Context, chain string, votes []uint64, valPower sdk.Int) error
 
 	// QueryPoll handles [server.QueryPoll]
 	QueryPoll(ctx sdk.Context, chain string, id uint64) (*types.Poll, error)
@@ -22,13 +26,98 @@ type PollKeeper interface {
 	QueryPolls(ctx sdk.Context, chain string, pageReq *query.PageRequest) ([]mitotypes.KV[uint64, *types.Poll], *query.PageResponse, error)
 }
 
-func (k keeper) SubmitPoll(ctx sdk.Context, val sdk.ValAddress, chain string, events []*types.Event) (uint64, error) {
-	// TODO
-	return 0, nil
+func (k keeper) FilterNewPolls(ctx sdk.Context, chain string, polls []*types.Poll) ([]*types.Poll, []mitotypes.KV[uint64, []byte], error) {
+	chainRepo := state.NewKVChainRepo(k.cdc, ctx.KVStore(k.storeKey))
+	chainPrefix, err := chainRepo.Load(chain)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var (
+		repo   = state.NewKVPollRepo(k.cdc, chainPrefix, ctx.KVStore(k.storeKey))
+		news   []*types.Poll
+		exists []mitotypes.KV[uint64, []byte]
+	)
+	for _, poll := range polls {
+		checksum, err := poll.GetPayload().Hash()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		loaded, err := repo.LoadByHash(checksum)
+		if err != nil {
+			return nil, nil, err
+		}
+		if loaded == nil {
+			news = append(news, loaded)
+		} else {
+			exists = append(exists, mitotypes.NewKV(loaded.GetId(), checksum))
+		}
+	}
+
+	return news, exists, nil
 }
 
-func (k keeper) VotePoll(ctx sdk.Context, val sdk.ValAddress, chain string, ids []uint64) error {
-	// TODO
+func (k keeper) SubmitPolls(
+	ctx sdk.Context,
+	chain string,
+	polls []*types.Poll,
+	totalPower sdk.Int,
+	valPower sdk.Int,
+) ([]mitotypes.KV[uint64, []byte], error) {
+	chainRepo := state.NewKVChainRepo(k.cdc, ctx.KVStore(k.storeKey))
+	chainPrefix, err := chainRepo.Load(chain)
+	if err != nil {
+		return nil, err
+	}
+
+	pollRepo := state.NewKVPollRepo(k.cdc, chainPrefix, ctx.KVStore(k.storeKey))
+
+	set := make([]mitotypes.KV[uint64, []byte], len(polls))
+	for i, poll := range polls {
+		// initialize poll
+		poll.Status = types.Poll_StatusPending
+		poll.Tally = &types.Tally{
+			TotalPower: &totalPower,
+			Confirmed:  &valPower,
+		}
+
+		pollId, err := pollRepo.Save(*poll)
+		if err != nil {
+			return nil, err
+		}
+
+		checksum, err := poll.GetPayload().Hash()
+		if err != nil {
+			return nil, err
+		}
+
+		set[i] = mitotypes.NewKV(pollId, checksum)
+	}
+
+	return set, nil
+}
+
+func (k keeper) VotePolls(ctx sdk.Context, chain string, votes []uint64, valPower sdk.Int) error {
+	chainRepo := state.NewKVChainRepo(k.cdc, ctx.KVStore(k.storeKey))
+	chainPrefix, err := chainRepo.Load(chain)
+	if err != nil {
+		return err
+	}
+
+	pollRepo := state.NewKVPollRepo(k.cdc, chainPrefix, ctx.KVStore(k.storeKey))
+	for _, id := range votes {
+		loaded, err := pollRepo.Load(id)
+		if err != nil {
+			return err
+		}
+		if loaded == nil {
+			return errors.ErrKeyNotFound
+		}
+
+		loaded.Tally.Confirmed = mitotypes.Ref(loaded.Tally.Confirmed.Add(valPower))
+	}
+
 	return nil
 }
 
