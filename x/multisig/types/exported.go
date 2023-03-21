44f8d316ba62289720230bede37e837696d53a23
hashableric <hashableric@gmail.com>
2023-03-21 21:18:27 +0900

    feat: add all validations

--- x/multisig/types/exported.go
+++ x/multisig/types/exported.go
@@ -1,7 +1,9 @@
 package types
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/btcsuite/btcd/btcec"
 	"github.com/many-things/mitosis/pkg/utils"
 	"strconv"
 	"strings"
@@ -11,6 +13,7 @@ type KeyID string
 type PublicKey []byte
 type Hash []byte
 type Signature []byte
+type SigID string
 
 var (
 	KeyIDMinLength = 4
@@ -41,4 +44,38 @@ func (k KeyID) ToInternalVariables() (string, uint64, error) {
 	}
 
 	return chainId, id, nil
+}
+
+func (s SigID) ValidateBasic() error {
+	if err := utils.ValidateString(string(s)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ToInternalVariables is make keyId into internal chainId / keyId
+func (s SigID) ToInternalVariables() (string, uint64, error) {
+	// Expect KeyID format as chainName-KeyID
+	splVal := strings.Split(string(s), "-")
+	chainId := strings.Join(splVal[:len(splVal)-1], "-")
+	id, err := strconv.ParseUint(splVal[len(splVal)-1], 10, 64)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return chainId, id, nil
+}
+
+func (p PublicKey) ValidateBasic() error {
+	btcecPubKey, err := btcec.ParsePubKey(p, btcec.S256())
+	if err != nil {
+		return err
+	}
+
+	if !bytes.Equal(p, btcecPubKey.SerializeCompressed()) {
+		return fmt.Errorf("public key is not compressed")
+	}
+
+	return nil
 }
