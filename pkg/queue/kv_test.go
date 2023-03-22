d5c940fc198ec80a8be9640ab57a63d630773213
ByeongSu Hong <hong@byeongsu.dev>
2023-03-22 19:45:26 +0900

    queue: pagination (#97)

--- pkg/queue/kv_test.go
+++ pkg/queue/kv_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -27,11 +28,73 @@ func setupKVQueue[T Message](t *testing.T, constructor func() T) Queue[T] {
 }
 
 func TestKVQueue(t *testing.T) {
-	testQueue(
-		t,
-		setupKVQueue[Message](
-			t,
-			func() Message { return &TestMessage{} },
-		),
+	q := setupKVQueue[Message](t, ConstructTestMessage)
+
+	testQueue(t, q)
+}
+
+func TestKVQueue_Paginate(t *testing.T) {
+	var (
+		err error
+		q   = setupKVQueue[Message](t, ConstructTestMessage)
+		tms = MakeTestMessages(12)
 	)
+
+	_, err = q.Produce(tms...)
+	require.NoError(t, err)
+
+	_, err = q.Consume(2)
+	require.NoError(t, err)
+
+	{ // Test happy case
+		for _, c := range []bool{true, false} {
+			resp, err := q.Paginate(
+				&query.PageRequest{Reverse: c},
+				func(m Message, i uint64) error {
+					require.Less(t, i, uint64(len(tms)))
+					require.GreaterOrEqual(t, i, uint64(2))
+					return nil
+				},
+			)
+			require.NoError(t, err)
+			require.Nil(t, resp.NextKey)
+		}
+	}
+
+	{ // Test overflowed key
+		cs := []struct {
+			key     uint64
+			reverse bool
+		}{
+			{key: 1, reverse: false},
+			{key: 13, reverse: true},
+		}
+		for _, c := range cs {
+			_, err = q.Paginate(
+				&query.PageRequest{Key: sdk.Uint64ToBigEndian(c.key), Reverse: c.reverse},
+				func(m Message, i uint64) error { return nil },
+			)
+			require.Error(t, err, "key out of range")
+		}
+	}
+
+	{ // Test next key
+		cs := []struct {
+			key      uint64
+			limit    uint64
+			reverse  bool
+			expected uint64
+		}{
+			{key: 5, limit: 3, reverse: false, expected: 8},
+			{key: 8, limit: 3, reverse: true, expected: 5},
+		}
+		for _, c := range cs {
+			resp, err := q.Paginate(
+				&query.PageRequest{Key: sdk.Uint64ToBigEndian(c.key), Limit: c.limit, Reverse: c.reverse},
+				func(m Message, i uint64) error { return nil },
+			)
+			require.NoError(t, err)
+			require.Equal(t, sdk.Uint64ToBigEndian(c.expected), resp.NextKey)
+		}
+	}
 }
