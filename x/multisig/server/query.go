00d0487575b22f773410d1def7874362db8edabf
Eric <125242347+hashableric@users.noreply.github.com>
2023-03-22 16:46:55 +0900

    proto: add multisig query proto (#95)
    * feat: add templates
    
    * proto: add types_query server
    
    * proto: add generated go code
    
    * apply code reviews

--- x/multisig/server/query.go
+++ x/multisig/server/query.go
@@ -24,3 +24,43 @@ func (k queryServer) Params(goCtx context.Context, req *QueryParamsRequest) (*Qu
 
 	return &QueryParamsResponse{Params: k.GetParams(ctx)}, nil
 }
+
+func (k queryServer) Keygen(ctx context.Context, keygen *QueryKeygen) (*QueryKeygenResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k queryServer) KeygenList(ctx context.Context, list *QueryKeygenList) (*QueryKeygenListResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k queryServer) PubKey(ctx context.Context, key *QueryPubKey) (*QueryPubKeyResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k queryServer) PubKeyList(ctx context.Context, list *QueryPubKeyList) (*QueryPubKeyListResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k queryServer) Sign(ctx context.Context, sign *QuerySign) (*QuerySignResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k queryServer) SignList(ctx context.Context, list *QuerySignList) (*QuerySignListResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k queryServer) Signature(ctx context.Context, signature *QuerySignature) (*QuerySignatureResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k queryServer) SignatureList(ctx context.Context, list *QuerySignatureList) (*QuerySignatureListResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
