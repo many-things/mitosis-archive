6895b011f6fa7c31f3b1455576fa9f75433cdd80
hashableric <hashableric@gmail.com>
2023-03-26 12:43:29 +0900

    cfg: add mitosis

--- sidecar/config/config.go
+++ sidecar/config/config.go
@@ -9,9 +9,17 @@ type TofNConfig struct {
 	DialTimeout time.Duration `mapstrcture:"tofnd-dial-timeout"`
 }
 
+type TendermintConfig struct {
+	Host   string `mapstructure:"host"`
+	Port   int    `mapstructure:"port"`
+	Denom  string `mapstructure:"denom"`
+	Prefix string `mapstructure:"prefix"`
+}
+
 // SidecarConfig contains configuration for all Sidecar Program
 type SidecarConfig struct {
-	TofNConfig TofNConfig `mapstructure:"tofn"`
+	TofNConfig TofNConfig       `mapstructure:"tofn"`
+	MitoConfig TendermintConfig `mapstructure:"mito"`
 }
 
 func DefaultTofNConfig() TofNConfig {
@@ -22,8 +30,18 @@ func DefaultTofNConfig() TofNConfig {
 	}
 }
 
+func DefaultMitoConfig() TendermintConfig {
+	return TendermintConfig{
+		Host:   "localhost",
+		Port:   9090,
+		Denom:  "mito",
+		Prefix: "mito",
+	}
+}
+
 func DefaultSidecarConfig() SidecarConfig {
 	return SidecarConfig{
 		TofNConfig: DefaultTofNConfig(),
+		MitoConfig: DefaultMitoConfig(),
 	}
 }
