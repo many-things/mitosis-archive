07a4cf6f68c285cdc63f3e2004f6d18701dffd2e
hashableric <hashableric@gmail.com>
2023-03-29 17:10:34 +0900

    feat: add temp

---
+++ x/multisig/keeper/pubkey_test.go
@@ -0,0 +1,19 @@
+package keeper_test
+
+import "testing"
+
+func Test_RegisterPubKey(t *testing.T) {
+	// TODO: implements
+}
+
+func Test_RemovePubKey(t *testing.T) {
+	// TODO: implements
+}
+
+func Test_QueryPubKey(t *testing.T) {
+	// TODO: implements
+}
+
+func Test_QueryPubKeyList(t *testing.T) {
+	// TODO: implements
+}
