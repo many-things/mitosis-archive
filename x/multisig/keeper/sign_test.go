68ef32a0ff57e3a28ccc3530e44e41109cf7b251
hashableric <hashableric@gmail.com>
2023-03-29 20:51:25 +0900

    feat: query sign

--- x/multisig/keeper/sign_test.go
+++ x/multisig/keeper/sign_test.go
@@ -86,8 +86,29 @@ func Test_UpdateSignStatus(t *testing.T) {
 	assert.Equal(t, updated.Status, types.Sign_StatusComplete)
 }
 
-func Test_QuerySign(_ *testing.T) {
-	// TODO: implements
+func Test_QuerySign(t *testing.T) {
+	k, ctx, cdc, storeKey := testkeeper.MultisigKeeper(t)
+	repo := state.NewKVChainSignRepo(cdc, ctx.KVStore(storeKey), chainID)
+	valAddr := genValAddr(t)
+
+	// try to query not exist sign event
+	_, err := k.QuerySign(ctx, chainID, 0)
+	assert.Error(t, err, "sign: not found")
+
+	// try to query exist sign event
+	sign := types.Sign{
+		Chain:         chainID,
+		SigID:         0,
+		KeyID:         "1",
+		Participants:  []sdk.ValAddress{valAddr},
+		MessageToSign: []byte("test"),
+		Status:        types.Sign_StatusAssign,
+	}
+	_ = repo.Save(&sign)
+
+	res, err := k.QuerySign(ctx, chainID, 0)
+	assert.NilError(t, err)
+	require.Equal(t, res, &sign)
 }
 
 func Test_QuerySignList(_ *testing.T) {
