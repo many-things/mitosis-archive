fc9a383f48588c7353428098bd6fd40e90fd5831
hashableric <hashableric@gmail.com>
2023-03-29 20:18:26 +0900

    feat: add sign test templates

---
+++ x/multisig/keeper/sign_test.go
@@ -0,0 +1,23 @@
+package keeper_test
+
+import "testing"
+
+func Test_RegisterSignEvent(_ *testing.T) {
+	// TODO: implements
+}
+
+func Test_RemoveSignEvent(_ *testing.T) {
+	// TODO: implements
+}
+
+func Test_UpdateSignStatus(_ *testing.T) {
+	// TODO: implements
+}
+
+func Test_QuerySign(_ *testing.T) {
+	// TODO: implements
+}
+
+func Test_QuerySignList(_ *testing.T) {
+	// TODO: implements
+}
