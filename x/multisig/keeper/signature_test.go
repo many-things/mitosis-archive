bc9aae66f3ae7f6d7c155743792b8ad303569e87
hashableric <hashableric@gmail.com>
2023-03-29 21:08:23 +0900

    feat: signature template

---
+++ x/multisig/keeper/signature_test.go
@@ -0,0 +1,19 @@
+package keeper_test
+
+import "testing"
+
+func Test_RegisterSignature(_ *testing.T) {
+	// TODO: implements
+}
+
+func Test_RemoveSignature(_ *testing.T) {
+	// TODO: implements
+}
+
+func Test_QuerySignature(_ *testing.T) {
+	// TODO: implements
+}
+
+func Test_QuerySignatureList(_ *testing.T) {
+	// TODO: implements
+}
