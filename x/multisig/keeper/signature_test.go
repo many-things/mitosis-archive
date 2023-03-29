d085401747b66261f51b6c7480cd8988a6c08c27
hashableric <hashableric@gmail.com>
2023-03-29 21:16:15 +0900

    feat: register signature

--- x/multisig/keeper/signature_test.go
+++ x/multisig/keeper/signature_test.go
@@ -1,9 +1,27 @@
 package keeper_test
 
-import "testing"
+import (
+	"testing"
 
-func Test_RegisterSignature(_ *testing.T) {
-	// TODO: implements
+	testkeeper "github.com/many-things/mitosis/testutil/keeper"
+	"github.com/many-things/mitosis/x/multisig/keeper/state"
+	"github.com/many-things/mitosis/x/multisig/types"
+	"gotest.tools/assert"
+)
+
+func Test_RegisterSignature(t *testing.T) {
+	k, ctx, cdc, storeKey := testkeeper.MultisigKeeper(t)
+	repo := state.NewKVChainSignatureRepo(cdc, ctx.KVStore(storeKey), chainID)
+	valAddr := genValAddr(t)
+
+	sig := types.Signature("signature")
+	err := k.RegisterSignature(ctx, chainID, 0, valAddr, sig)
+	assert.NilError(t, err)
+
+	// validate registered successfully
+	res, err := repo.Load(0, valAddr)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, sig, res)
 }
 
 func Test_RemoveSignature(_ *testing.T) {
