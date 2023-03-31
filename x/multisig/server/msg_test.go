4105ef1f3e82ae0ef3a4cd39b534e1e11f1adb48
hashableric <hashableric@gmail.com>
2023-03-31 22:44:34 +0900

    feat: add template

--- x/multisig/server/msg_test.go
+++ x/multisig/server/msg_test.go
@@ -12,3 +12,20 @@ func setupMsgServer(t testing.TB) (MsgServer, context.Context) {
 	k, ctx, _, _ := keepertest.MultisigKeeper(t)
 	return NewMsgServer(k), sdk.WrapSDKContext(ctx)
 }
+
+func Test_StartKeygen_Success(t *testing.T) {
+	// TODO: implements
+
+}
+
+func Test_StartKeygen_Failure(t *testing.T) {
+	// TODO: impelment
+}
+
+func Test_SubmitPubKey(t *testing.T) {
+	// TODO: implement
+}
+
+func Test_SubmitSignature(t *testing.T) {
+	// TODO: implement
+}
