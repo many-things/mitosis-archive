eec91f0638e8a5d63936722f5e02672ccc1576d4
hashableric <hashableric@gmail.com>
2023-04-02 23:55:39 +0900

    feat: keeper

--- x/event/keeper/state/tx_payload.go
+++ x/event/keeper/state/tx_payload.go
@@ -4,17 +4,19 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/store"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/many-things/mitosis/x/event/types"
 )
 
 type TxPayloadRepo interface {
-	Load(pollID uint64) (types.MsgHash, error)
-	Create(pollID uint64, hash types.MsgHash) error
+	Load(pollID uint64) (types.MsgHash, bool)
+	Has(pollID uint64) bool
 	Save(pollID uint64, hash types.MsgHash) error
 	Delete(pollID uint64) error
 
-	ExportGenesis() (*types.GenesisProxy, error)
-	ImportGenesis(genState *types.GenesisProxy) error
+	ExportGenesis() (*types.GenesisTxPayload_ChainSet, error)
+	ImportGenesis(genState *types.GenesisTxPayload_ChainSet) error
 }
 
 var (
@@ -22,37 +24,73 @@ var (
 )
 
 type kvTxPayloadRepo struct {
-	cdc  codec.BinaryCodec
-	root store.KVStore
+	cdc   codec.BinaryCodec
+	root  store.KVStore
+	chain string
 }
 
 func NewKVTxPayloadRepo(cdc codec.BinaryCodec, store store.KVStore, chain string) TxPayloadRepo {
 	return kvTxPayloadRepo{
-		cdc:  cdc,
-		root: prefix.NewStore(store, append(kvProxyRepoKey, []byte(chain)...)),
+		cdc:   cdc,
+		root:  prefix.NewStore(store, append(kvProxyRepoKey, []byte(chain)...)),
+		chain: chain,
 	}
 }
 
-func (k kvTxPayloadRepo) Load(pollID uint64) (types.MsgHash, error) {
-	return nil, nil
+func (k kvTxPayloadRepo) Load(pollID uint64) (types.MsgHash, bool) {
+	h := prefix.NewStore(k.root, kvTxPayloadItemPrefix).Get(sdk.Uint64ToBigEndian(pollID))
+	return h, h != nil
 }
 
-func (k kvTxPayloadRepo) Create(pollID uint64, hash types.MsgHash) error {
-	return nil
+func (k kvTxPayloadRepo) Has(pollID uint64) bool {
+	return prefix.NewStore(k.root, kvTxPayloadItemPrefix).Has(sdk.Uint64ToBigEndian(pollID))
 }
 
 func (k kvTxPayloadRepo) Save(pollID uint64, hash types.MsgHash) error {
+	prefix.NewStore(k.root, kvTxPayloadItemPrefix).Set(sdk.Uint64ToBigEndian(pollID), hash)
 	return nil
 }
 
 func (k kvTxPayloadRepo) Delete(pollID uint64) error {
+	prefix.NewStore(k.root, kvTxPayloadItemPrefix).Delete(sdk.Uint64ToBigEndian(pollID))
 	return nil
 }
 
-func (k kvTxPayloadRepo) ExportGenesis() (*types.GenesisProxy, error) {
-	return nil, nil
+func (k kvTxPayloadRepo) ExportGenesis() (*types.GenesisTxPayload_ChainSet, error) {
+	var items []*types.GenesisTxPayload_ItemSet
+
+	_, err := query.Paginate(
+		prefix.NewStore(k.root, kvTxPayloadItemPrefix),
+		&query.PageRequest{Limit: query.MaxLimit},
+		func(key []byte, value []byte) error {
+			items = append(
+				items,
+				&types.GenesisTxPayload_ItemSet{
+					PollId:    sdk.BigEndianToUint64(key),
+					TxPayload: value,
+				})
+			return nil
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	} else if len(items) == 0 {
+		return nil, nil
+	}
+
+	return &types.GenesisTxPayload_ChainSet{
+		Chain:   []byte(k.chain),
+		ItemSet: items,
+	}, nil
 }
 
-func (k kvTxPayloadRepo) ImportGenesis(genState *types.GenesisPayload) {
+func (k kvTxPayloadRepo) ImportGenesis(genState *types.GenesisTxPayload_ChainSet) error {
+	s := prefix.NewStore(k.root, kvTxPayloadItemPrefix)
 
+	for _, item := range genState.GetItemSet() {
+		s.Set(sdk.Uint64ToBigEndian(item.PollId), item.TxPayload)
+	}
+
+	return nil
 }
