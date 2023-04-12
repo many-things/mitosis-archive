3b7206fe9c7213e73ecc7fe46afc5f699a5903af
hashableric <hashableric@gmail.com>
2023-04-13 01:30:52 +0900

    feat: broadcast message

--- sidecar/tofnd/session/keygen.go
+++ sidecar/tofnd/session/keygen.go
@@ -1,13 +1,21 @@
 package session
 
 import (
+	"context"
+	"fmt"
 	"sync"
 
 	"github.com/many-things/mitosis/sidecar/config"
 	"github.com/many-things/mitosis/sidecar/types"
+	"google.golang.org/grpc"
 )
 
 type KeygenSession interface {
+	StartSession() error
+	CloseSession() error
+	BroadcastMsg(msg types.TrafficIn) error
+	ConsumeMsg(msg types.TrafficIn) error
+	IsRunning() bool
 }
 
 type keygenSessionMgr struct {
@@ -15,9 +23,10 @@ type keygenSessionMgr struct {
 }
 
 type keygenSession struct {
-	Config   config.SidecarConfig
-	msg      types.KeygenInit
-	sessions map[string]types.SidecarClient
+	config    config.TofNConfig
+	msg       types.KeygenInit
+	sessions  map[string]*grpc.ClientConn
+	isRunning bool
 }
 
 var mgrInstance *keygenSessionMgr
@@ -37,11 +46,11 @@ func GetInstance() *keygenSessionMgr { //nolint: revive
 	return mgrInstance
 }
 
-func (m *keygenSessionMgr) CreateSession(cfg config.SidecarConfig, msg types.KeygenInit) KeygenSession {
+func (m *keygenSessionMgr) CreateSession(cfg config.TofNConfig, msg types.KeygenInit) KeygenSession {
 	m.sessions[msg.NewKeyUid] = &keygenSession{
-		Config:   cfg,
+		config:   cfg,
 		msg:      msg,
-		sessions: map[string]types.SidecarClient{},
+		sessions: map[string]*grpc.ClientConn{},
 	}
 
 	return m.sessions[msg.NewKeyUid]
@@ -56,6 +65,93 @@ func (m *keygenSessionMgr) GetSession(key string) (KeygenSession, bool) {
 	return sess, true
 }
 
-func (m *keygenSessionMgr) ReleaseSession(key string) {
-	delete(m.sessions, key)
+func (m *keygenSessionMgr) Consume(msg types.ShareKeygenRequest) error {
+	sess, ok := m.GetSession(msg.NewKeyUid)
+	if !ok {
+		return fmt.Errorf("key_uid not found: %s", msg.NewKeyUid)
+	}
+
+	if !sess.IsRunning() {
+		return fmt.Errorf("key_uid not initialized: %s", msg.NewKeyUid)
+	}
+
+	return sess.ConsumeMsg(*msg.Traffic)
+}
+
+func (m *keygenSessionMgr) ReleaseSession(key string) error {
+	if sess, ok := m.GetSession(key); ok {
+		if sess.IsRunning() {
+			err := sess.CloseSession()
+			if err != nil {
+				return err
+			}
+		}
+		delete(m.sessions, key)
+	}
+
+	return nil
+}
+
+func (s *keygenSession) StartSession() error {
+	// convert node list to HashMap
+	nodeMap := map[string]string{}
+	for _, v := range s.config.Nodes {
+		nodeMap[v.Validator] = v.Host
+	}
+
+	for _, p := range s.msg.PartyUids {
+		if p == s.config.Validator {
+			continue
+		}
+
+		dialURL := nodeMap[p]
+		conn, err := grpc.Dial(dialURL, grpc.WithInsecure()) // nolint: staticcheck
+		if err != nil {
+			return err
+		}
+
+		s.sessions[p] = conn
+	}
+
+	s.isRunning = true
+
+	return nil
+}
+
+func (s *keygenSession) CloseSession() error {
+	// TODO: apply mutex
+	for _, p := range s.sessions {
+		err := p.Close()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	s.isRunning = false
+	return nil
+}
+
+func (s *keygenSession) BroadcastMsg(msg types.TrafficIn) error {
+	for _, v := range s.sessions {
+		serv := types.NewSidecarClient(v)
+		_, err := serv.ShareKeygenTraffic(context.Background(), &types.ShareKeygenRequest{
+			NewKeyUid: s.msg.GetNewKeyUid(),
+			Traffic:   &msg,
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *keygenSession) ConsumeMsg(_ types.TrafficIn) error {
+	panic("implement me")
+}
+
+func (s *keygenSession) IsRunning() bool {
+	return s.isRunning
 }
