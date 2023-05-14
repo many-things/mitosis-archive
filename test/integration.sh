649a879837ea166fa35ba4faf256a6e9b1f9b14a
byeongsu-hong <hong@byeongsu.dev>
2023-05-14 17:12:34 +0900

    all in one

--- test/integration.sh
+++ test/integration.sh
@@ -10,6 +10,54 @@ DAEMON=${DAEMON:-"./build/mitosisd --home ./test/localnet --keyring-backend file
 
 ACCOUNT_ADDR=$(echo "mitomito" | $DAEMON keys show $ACCOUNT_NAME -a)
 
+cat $(file "register-proxy.json") \
+  | jq '.validator="mitovaloper127rnkklgkc5alfgtzlv2mk4capr4kdc30a36tn"' \
+  | jq '.proxy_account="'$ACCOUNT_ADDR'"' \
+  > $(file "temp.json")
+
+echo "mitomito" \
+  | $DAEMON tx event register-proxy $(file "temp.json") \
+    --chain-id 'mito-local-1' \
+    --from $ACCOUNT_NAME \
+    --fees '2000umito' \
+    --log_level 'trace' \
+    --output json \
+    -b 'block' -y \
+  | jq \
+  > $(file "register-proxy.resp.json")
+
+cat $(file "register-chain.json") \
+  | jq '.sender = "'$ACCOUNT_ADDR'"' \
+  | jq '.chain="osmosis"' \
+  > $(file "temp.json")
+
+echo "mitomito" \
+  | $DAEMON tx event register-chain $(file "temp.json") \
+    --chain-id 'mito-local-1' \
+    --from $ACCOUNT_NAME \
+    --fees '2000umito' \
+    --log_level 'trace' \
+    --output json \
+    -b 'block' -y \
+  | jq \
+  > $(file "register-chain-osmosis.resp.json")
+
+cat $(file "register-chain.json") \
+  | jq '.sender = "'$ACCOUNT_ADDR'"' \
+  | jq '.chain="ethereum"' \
+  > $(file "temp.json")
+
+echo "mitomito" \
+  | $DAEMON tx event register-chain $(file "temp.json") \
+    --chain-id 'mito-local-1' \
+    --from $ACCOUNT_NAME \
+    --fees '2000umito' \
+    --log_level 'trace' \
+    --output json \
+    -b 'block' -y \
+  | jq \
+  > $(file "register-chain-ethereum.resp.json")
+
 cat $(file "submit-event.json") \
   | jq '.sender = "'$ACCOUNT_ADDR'"' \
   > $(file "temp.json")
