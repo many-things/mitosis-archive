e3a78852ddc4f20774eabdf3f40c017cb9dbabfe
byeongsu-hong <hong@byeongsu.dev>
2023-05-14 17:34:12 +0900

    add signer handler

--- x/context/server/msg.go
+++ x/context/server/msg.go
@@ -1,6 +1,11 @@
 package server
 
-import "github.com/many-things/mitosis/x/context/keeper"
+import (
+	"context"
+	sdkerrutils "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/many-things/mitosis/x/context/keeper"
+)
 
 type msgServer struct {
 	keeper.Keeper
@@ -13,3 +18,33 @@ func NewMsgServer(keeper keeper.Keeper) MsgServer {
 }
 
 var _ MsgServer = msgServer{}
+
+func (m msgServer) SignerReady(ctx context.Context, msg *MsgSignerReady) (*MsgSignerReadyResponse, error) {
+	wctx := sdk.UnwrapSDKContext(ctx)
+
+	if err := m.Keeper.SetReadyToSigner(wctx, msg.GetChain()); err != nil {
+		return nil, sdkerrutils.Wrap(err, "internal")
+	}
+
+	return &MsgSignerReadyResponse{}, nil
+}
+
+func (m msgServer) RegisterCosmosSigner(ctx context.Context, msg *MsgRegisterCosmosSigner) (*MsgRegisterCosmosSignerResponse, error) {
+	wctx := sdk.UnwrapSDKContext(ctx)
+
+	if err := m.Keeper.RegisterCosmosSigner(wctx, msg.GetChain(), msg.GetPubKey(), msg.GetAccountNumber()); err != nil {
+		return nil, sdkerrutils.Wrap(err, "internal")
+	}
+
+	return &MsgRegisterCosmosSignerResponse{}, nil
+}
+
+func (m msgServer) RegisterEVMSigner(ctx context.Context, msg *MsgRegisterEVMSigner) (*MsgRegisterEVMSignerResponse, error) {
+	wctx := sdk.UnwrapSDKContext(ctx)
+
+	if err := m.Keeper.RegisterEVMSigner(wctx, msg.GetChain(), msg.GetPubKey()); err != nil {
+		return nil, sdkerrutils.Wrap(err, "internal")
+	}
+
+	return &MsgRegisterEVMSignerResponse{}, nil
+}
