796bf2fb04323e2aeccc13002606135aa3c1e83d
byeongsu-hong <hong@byeongsu.dev>
2023-05-15 01:42:32 +0900

    start & submit

--- test/integration-multisig.sh
+++ test/integration-multisig.sh
@@ -4,13 +4,38 @@ function file() {
   echo "./test/$1"
 }
 
-ACCOUNT_NAME="validator"
+SIGNER_NAME=${SIGNER_NAME:-"signer"}
+VALIDATOR_NAME=${VALIDATOR_NAME:-"validator"}
+DAEMON=${DAEMON:-"./build/mitosisd --home ./test/localnet"}
 
-DAEMON=${DAEMON:-"./build/mitosisd --home ./test/localnet --keyring-backend file"}
+SIGNER_ADDR=$(echo "mitomito" | $DAEMON keys show $SIGNER_NAME -a --keyring-backend file)
 
-ACCOUNT_ADDR=$(echo "mitomito" | $DAEMON keys show $ACCOUNT_NAME -a)
+$DAEMON tx bank send $SIGNER_ADDR $SIGNER_ADDR 1umito --fees 2000umito --generate-only | jq > $(file "temp-tx.json")
+$(file broadcast.sh) $SIGNER_NAME "self-send"
 
-# TODO
+SIGNER_INFO=$($DAEMON q account $SIGNER_ADDR --output json | jq -c)
+VALIDATOR_ADDR=$(echo "mitomito" | $DAEMON keys show $VALIDATOR_NAME -a --keyring-backend file)
+
+cat $(file "start-keygen.json") \
+  | jq '.key_id="osmo-test-5-0"' \
+  | jq '.participants=["mitovaloper127rnkklgkc5alfgtzlv2mk4capr4kdc30a36tn"]' \
+  | jq '.sender="'$VALIDATOR_ADDR'"' \
+  > $(file "temp.json")
+
+$DAEMON tx multisig start-keygen $(file "temp.json") --fees 2000umito --generate-only | jq > $(file "temp-tx.json")
+$(file broadcast.sh) $VALIDATOR_NAME "start-keygen"
+echo "keygen started"
+
+cat $(file "submit-pubkey.json") \
+  | jq '.key_id="osmo-test-5-0"' \
+  | jq '.participant="mitovaloper127rnkklgkc5alfgtzlv2mk4capr4kdc30a36tn"' \
+  | jq '.pub_key="'$(echo "$SIGNER_INFO" | jq -r ".pub_key.key")'"' \
+  | jq '.sender="'$VALIDATOR_ADDR'"' \
+  > $(file "temp.json")
+
+$DAEMON tx multisig submit-pubkey $(file "temp.json") --fees 2000umito --generate-only | jq > $(file "temp-tx.json")
+$(file broadcast.sh) $VALIDATOR_NAME "submit-pubkey"
+echo "pubkey submitted"
 
 rm $(file "temp.json")
 rm $(file "temp-tx.json")
--- test/integration-signer.sh
+++ test/integration-signer.sh
@@ -4,16 +4,11 @@ function file() {
   echo "./test/$1"
 }
 
-
 SIGNER_NAME=${SIGNER_NAME:-"signer"}
 VALIDATOR_NAME=${VALIDATOR_NAME:-"validator"}
 DAEMON=${DAEMON:-"./build/mitosisd --home ./test/localnet"}
 
 SIGNER_ADDR=$(echo "mitomito" | $DAEMON keys show $SIGNER_NAME -a --keyring-backend file)
-
-$DAEMON tx bank send $SIGNER_ADDR $SIGNER_ADDR 1umito --fees 2000umito --generate-only | jq > $(file "temp-tx.json")
-$(file broadcast.sh) $SIGNER_NAME "self-send"
-
 SIGNER_INFO=$($DAEMON q account $SIGNER_ADDR --output json | jq -c)
 VALIDATOR_ADDR=$(echo "mitomito" | $DAEMON keys show $VALIDATOR_NAME -a --keyring-backend file)
 
@@ -24,7 +19,7 @@ cat $(file "register-cosmos-signer.json") \
   | jq '.account_number="'$(echo "$SIGNER_INFO" | jq -r ".account_number")'"' \
   | jq '.sequence="'$(echo "$SIGNER_INFO" | jq -r ".sequence")'"' \
   > $(file "temp.json")
-wwwwwwww
+
 $DAEMON tx context register-cosmos-signer $(file "temp.json") --fees 2000umito --generate-only | jq > $(file "temp-tx.json")
 $(file broadcast.sh) $VALIDATOR_NAME "register-cosmos-signer"
 echo "cosmos signer registered"
